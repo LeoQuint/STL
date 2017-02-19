78e1c9786f1d1a4ad3d32f4d363af7ae05f333e0
Leonard <leonardquintal@gmail.com>
2017-02-19 10:53:22 -0500

    local setup

---
+++ Assets/Scripts/Player/SetLocalPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+public class SetLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (isLocalPlayer) {
+            GetComponent<PlayerController>().enabled = true;
+        }
+	}
+	
+}
