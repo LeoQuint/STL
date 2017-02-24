6cc1dd68a35a83ae07e9be1d28622f68a02cf00e
Leonard <leonardquintal@gmail.com>
2017-02-24 17:32:02 -0500

    level

--- Assets/Scripts/UI/UI_Player_Info.cs
+++ Assets/Scripts/UI/UI_Player_Info.cs
@@ -8,11 +8,13 @@ public class UI_Player_Info : MonoBehaviour {
     private Text m_UI_Player_Info_Text;
     // Use this for initialization
     void Start () {
+        
         //The ui element is located on the main canvas.
         m_UI_Player_Info_Text = GameObject.FindGameObjectWithTag("UI")
                     .transform.FindChild("playerNamesDisplay")
                     .transform.FindChild( transform.GetComponentInParent<PlayerController>().m_PlayerNumber.ToString() )
                     .GetComponent<Text>();
+
         m_UI_Player_Info_Text.gameObject.SetActive(true);
     }
 	
