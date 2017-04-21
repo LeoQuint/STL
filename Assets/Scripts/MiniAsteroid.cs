4ebcdbf8e303772cd9545bfc12c1c08a37ccd524
Eric Blanchard <Eric.x2@gmail.com>
2017-04-20 22:51:49 -0400

    Mini asteroid update

--- Assets/Scripts/MiniAsteroid.cs
+++ Assets/Scripts/MiniAsteroid.cs
@@ -6,9 +6,32 @@ public class MiniAsteroid : MonoBehaviour {
 
     private float speed = 10f;
 
+    SoundManager sm;
+
     private void Awake()
     {
         Destroy(gameObject, 10);
         GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f))*speed;
+        sm = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ship")
+        {
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject p in players)
+            {
+                if (p.GetComponent<STL_PlayerController>().enabled)
+                {
+                    p.GetComponent<STL_PlayerController>().ShipHealth -= 3f;
+                }
+            }
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.tag != "MiniAsteroid")
+        {
+            Destroy(gameObject);
+        }
     }
 }
