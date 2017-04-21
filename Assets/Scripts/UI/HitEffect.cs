4161b4b243a207bc49b072ddc39d8e7b31fab402
OverG0d <ardainan196@gmail.com>
2017-04-20 21:18:06 -0400

    Hit plane for when inside ship

---
+++ Assets/Scripts/UI/HitEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitEffect : MonoBehaviour {
+    public GameObject plane;
+    public bool activate = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(activate)
+        {
+            activate = false;
+            Enable();
+        }
+	}
+    void Enable()
+    {
+        plane.SetActive(true);
+        StartCoroutine("Disable");
+    }
+    IEnumerator Disable()
+    {
+        yield return new WaitForSeconds(3.0f);
+        plane.SetActive(false);
+    }
+}
